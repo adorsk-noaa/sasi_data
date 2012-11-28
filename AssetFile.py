c2960daabf8fc1dde83f732dad53cc7f234b40f7
adorsk <adorsk>
2012-11-28 16:01:31 -0500

    requirements files

---
+++ AssetFile.py
@@ -0,0 +1,23 @@
+config = {
+    'CACHE_DIR': 'cache',
+    'TARGET_DIR': 'assets'
+}
+
+assets = {
+    'sa_dao' : {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/sqlalchemy_dao.git',
+    },
+    'sasi_data' : {
+        'type': 'git',
+        'source': 'https://github.com/adorsk-noaa/sasi_data',
+    },
+    'sqlalchemy' : {
+        'type': 'hg',
+        'source': 'https://adorsk@bitbucket.org/adorsk/sqlalchemy',
+    },
+    'geoalchemy': {
+        'type': 'git',
+        'source': 'https://github.com/adorsk/geoalchemy.git',
+    },
+}

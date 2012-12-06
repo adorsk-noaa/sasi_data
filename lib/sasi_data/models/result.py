42f184f533a31916751bb3797800923ace4350ba
adorsk <adorsk>
2012-12-06 18:20:16 -0500

    added feature category to result

--- lib/sasi_data/models/result.py
+++ lib/sasi_data/models/result.py
@@ -1,6 +1,9 @@
 class Result(object):
 
-    def __init__(self, id=None, t=None, cell_id=None, gear_id=None, substrate_id=None, energy_id=None, feature_id = None, a=None, x=None, y=None, z=None, znet=None):
+    def __init__(self, id=None, t=None, cell_id=None, gear_id=None,
+                 substrate_id=None, energy_id=None, feature_id=None,
+                 feature_category_id=None, a=None, x=None, y=None, z=None, 
+                 znet=None):
         self.id = id
         self.t = t
         self.cell_id = cell_id
@@ -8,6 +11,7 @@ class Result(object):
         self.substrate_id = substrate_id
         self.energy_id = energy_id
         self.feature_id = feature_id
+        self.feature_category_id = feature_category_id
         self.a = a
         self.x = x
         self.y = y

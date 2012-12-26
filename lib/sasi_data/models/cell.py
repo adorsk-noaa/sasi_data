075e92bc0325ee6503a8af4660b1d7a970927b9b
adorsk <adorsk>
2012-12-26 18:07:37 -0500

    added depth to cell

--- lib/sasi_data/models/cell.py
+++ lib/sasi_data/models/cell.py
@@ -1,8 +1,10 @@
 class Cell(object):
-    def __init__(self, id=None, type=None, type_id=None, geom=None, area=None, habitat_composition=None):
+    def __init__(self, id=None, type=None, type_id=None, geom=None, area=None,
+                 habitat_composition=None, z=None):
         self.id = id
         self.type = type
         self.type_id = type_id
         self.geom = geom
         self.area = area
         self.habitat_composition = habitat_composition
+        self.z = z
